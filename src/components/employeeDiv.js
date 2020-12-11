a8892e6d705ff9e4f9ab06acced31fd31161d34a
jewel-7 <jewel.simon@raweng.com>
2020-12-12 02:07:55 +0530

    added more content

--- src/components/employeeDiv.js
+++ src/components/employeeDiv.js
@@ -1,10 +1,10 @@
 export const renderEmployeeCard = (emp) => {
-    const card = document.createElement("div");
-    card.innerHTML = `<div class="employee-image"> 
+  const card = document.createElement("div");
+  card.innerHTML = `<div class="employee-image"> 
     <img src="${emp.imageUrl}"alt ="Employee Image"/> 
     </div> 
     <p id="emp${emp.id}">${emp.firstName} ${emp.lastName}</p> 
-    <p>${emp.company}</p>`;
-    card.className = "employee-card";
-    return card;
+    <p>COMPANY:- ${emp.company}</p> <p>EMAIL:- ${emp.email}</p> <p>POSITION:- ${emp.position}</p> <p>SKILLS:- ${emp.skills}</p>  `;
+  card.className = "employee-card";
+  return card;
 };
--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
 let url =
   "https://niravkpatel28.github.io/json-data-server/employees/employees.json";
 
+// immediately invoked function expression IIFE
+
 export const getUsers = async (url) => {
   let response = await axios.get(url);
   console.log(response.data);
